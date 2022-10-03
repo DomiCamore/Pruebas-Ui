9fe032ba06288d092a947ad48063c524745a6ecf
Maribel <domicamore@gmail.com>
2022-10-03 11:26:00 -0500

    Font

--- SwiftUIPruebas/SwiftUIPruebas/ContentView.swift
+++ SwiftUIPruebas/SwiftUIPruebas/ContentView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Hola Mundo")
+            .fontWeight(.bold)
+            .font(.system(size:45, design: .rounded))
+            .foregroundColor(.red)
     }
 }
 

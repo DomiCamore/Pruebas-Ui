f9e1f115f6318e65d06254d360f82256c1f0ff69
Joseph Calla <josephestanislao9@gmail.com>
2022-10-03 10:11:39 -0500

    Inicio de Repo

---
+++ SwiftUIPruebas/SwiftUIPruebas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftUIPruebas
+//
+//  Created by Joseph Estanislao Calla Moreyra on 10/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

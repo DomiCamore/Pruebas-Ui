02042e64540d77a488a77130d371917c8192fa71
Maribel <domicamore@gmail.com>
2022-10-03 10:48:59 -0500

    cambio de proyecto

---
+++ SwiftUIPruebas/SwiftUIPruebas/SwiftUIPruebasApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPruebasApp.swift
+//  SwiftUIPruebas
+//
+//  Created by Joseph Estanislao Calla Moreyra on 10/3/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPruebasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

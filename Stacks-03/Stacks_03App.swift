e63f6c48e02c11d05bcada976598b5ab4961c217
Maribel <domicamore@gmail.com>
2022-10-05 14:49:20 -0500

    1era Tarea resuelta

---
+++ Stacks-03/Stacks_03App.swift
@@ -0,0 +1,17 @@
+//
+//  Stacks_03App.swift
+//  Stacks-03
+//
+//  Created by Joseph Estanislao Calla Moreyra on 10/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Stacks_03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

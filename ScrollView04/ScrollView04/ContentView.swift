d6b198622a981f14f718dcbc68df4483e43cc0b4
Miguel O <miguel-otero-calderon@hotmail.com>
2022-10-11 10:29:24 -0500

    Carrusel y titulo terminado scrollView

--- ScrollView04/ScrollView04/ContentView.swift
+++ ScrollView04/ScrollView04/ContentView.swift
@@ -12,44 +12,58 @@ struct ContentView: View {
        
        
             //efecto carrousel (.horizontal),Hstack, frame,
-        ScrollView(.horizontal) {
+        ScrollView {
             
             VStack() {
+                Image("viajero")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 70)
+                    
+                   // .frame(width: 200)
+                 
+                
                 Text("Juan Gabriel Gomila")
                     .font(.system(.largeTitle,design:.rounded))
-                Image("machupicchu")
-                Text("Subtitulo")
-                    .foregroundColor(<#T##color: Color?##Color?#>)
-                Text("Subtitulo 2 ")
+                    .fontWeight(.bold)
+               //
+                Text("Uci Profesor Ceo Frogames")
+                    .foregroundColor(.red)
+                Text("Subtitulo Cantidad de estudiantes ")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                    .   fontWeight(.light)
+                    
+                
             }
             
                 
-                HStack {
+              
                     CardView(imageName: "machupicchu", authorName: "Juan Gabriel Gomila", coursetitle: "DISEÑO DE APP PARA IOS 13 CON SWIFT UI DESDE O", originalPrice: "14.00", discountPrice: "12.99")
-                        .frame(width:300)
+                       // .frame(width:300) se limita en carrusel
                     
                     
                     CardView(imageName: "machupicchu", authorName: "Juan Gabriel Gomila", coursetitle: "Curso de videojuegos unity  DESDE O", originalPrice: "14.00", discountPrice: "12.99")
-                        .frame(width:300)
+                    // .frame(width:300)se limita en carrusel
                     
                     
                     
                     CardView(imageName: "machupicchu", authorName: "Juan Gabriel Gomila", coursetitle: "curso de probalididad y variables DESDE O", originalPrice: "14.00", discountPrice: "12.99")
-                        .frame(width: 300)
+                    // .frame(width:300)se limita en carrusel
                     
                     
                     CardView(imageName: "machupicchu", authorName: "Juan Gabriel Gomila", coursetitle: "Curso de videojuegos unity  DESDE O", originalPrice: "14.00", discountPrice: "12.99")
-                        .frame(width:300)
+                    // .frame(width:300)se limita en carrusel
                     
                     CardView(imageName: "machupicchu", authorName: "Juan Gabriel Gomila", coursetitle: "Curso de videojuegos unity  DESDE O", originalPrice: "14.00", discountPrice: "12.99")
-                        .frame(width:300)
+                    // .frame(width:300)se limita en carrusel
                     
                     
                 }
             Spacer()
             }
      
-        }
+      
    
 
 

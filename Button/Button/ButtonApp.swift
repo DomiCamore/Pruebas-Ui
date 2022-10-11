104bf2c5ecb48282613d0658c51318ddff3f6991
Miguel O <miguel-otero-calderon@hotmail.com>
2022-10-11 16:42:39 -0500

    Boton con texto

---
+++ Button/Button/ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonApp.swift
+//  Button
+//
+//  Created by Wigilabs on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Button/Button/ContentView.swift
@@ -0,0 +1,73 @@
+//
+//  ContentView.swift
+//  Button
+//
+//  Created by on 11/10/22.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack{
+            VStack {
+                Button(action: {
+                    // QUE DEBE HACER EL BOTTON
+                    print("Hola Pulsado")
+                    
+                }) {
+                    // como debe verse el botton
+                    // texto estilo - ancho texto - padding
+                    // Color fondo - color de texto
+                    Text("Hola mundo!!")
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                    
+                        .padding()
+                        .background(.green)
+                        .foregroundColor(.white)
+                    
+                        .padding(10)
+                        .border(Color.green,width: 6)
+                    
+                    
+                }
+               
+            }
+            Button(action: {
+                // QUE DEBE HACER EL BOTTON
+                print("Hola Pulsado")
+                
+            }) {
+                // como debe verse el botton
+                // texto estilo - ancho texto - padding
+                // Color fondo - color de texto
+                Text("Hola mundo!!")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                
+                    .padding()
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(50)
+                
+                    .padding(12)
+                    .overlay (
+                   RoundedRectangle(cornerRadius: 50)
+//                   stroke (relleno)corte(va todo el interior)
+                .stroke(Color.green,lineWidth: 5)
+                    )
+                
+                
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

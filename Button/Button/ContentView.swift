c64810293dd9dc9cda31e543bde59d8ec7989279
Miguel O <miguel-otero-calderon@hotmail.com>
2022-10-12 10:46:31 -0500

    Boton

--- Button/Button/ContentView.swift
+++ Button/Button/ContentView.swift
@@ -11,55 +11,37 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        VStack{
+        VStack {
+            
+            
             VStack {
-                Button(action: {
-                    // QUE DEBE HACER EL BOTTON
-                    print("Hola Pulsado")
-                    
-                }) {
-                    // como debe verse el botton
-                    // texto estilo - ancho texto - padding
-                    // Color fondo - color de texto
-                    Text("Hola mundo!!")
-                        .font(.largeTitle)
-                        .fontWeight(.black)
-                    
-                        .padding()
-                        .background(.green)
-                        .foregroundColor(.white)
-                    
-                        .padding(10)
-                        .border(Color.green,width: 6)
-                    
-                    
+                VStack{
+                  
+                    Button(action: {
+                        // QUE DEBE HACER EL BOTTON <--
+                        print("Hola Pulsado")
+                        
+                    }) {
+                        // como debe verse el botton
+                        // texto estilo - ancho texto - padding
+                        // Color fondo - color de texto
+                        Text("Hola mundo!!")
+                            .font(.largeTitle)
+                            .fontWeight(.black)
+                        
+                            .padding()
+                            .background(Color.green)
+                            .foregroundColor(.white)
+                            .cornerRadius(50)
+                        
+                            .padding(12)
+                            .overlay (
+                           RoundedRectangle(cornerRadius: 50)
+                           // stroke (relleno)corte(va todo el interior)
+                        .stroke(Color.green,lineWidth: 5)
+                            )
+                    }
                 }
-               
-            }
-            Button(action: {
-                // QUE DEBE HACER EL BOTTON
-                print("Hola Pulsado")
-                
-            }) {
-                // como debe verse el botton
-                // texto estilo - ancho texto - padding
-                // Color fondo - color de texto
-                Text("Hola mundo!!")
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                
-                    .padding()
-                    .background(Color.green)
-                    .foregroundColor(.white)
-                    .cornerRadius(50)
-                
-                    .padding(12)
-                    .overlay (
-                   RoundedRectangle(cornerRadius: 50)
-//                   stroke (relleno)corte(va todo el interior)
-                .stroke(Color.green,lineWidth: 5)
-                    )
-                
                 
             }
         }

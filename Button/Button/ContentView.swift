27c5829e179bee940a7760bac7710c10ddb5e083
Miguel O <miguel-otero-calderon@hotmail.com>
2022-10-12 14:05:33 -0500

    Botones 4 Modelos

--- Button/Button/ContentView.swift
+++ Button/Button/ContentView.swift
@@ -16,7 +16,7 @@ struct ContentView: View {
             
             VStack {
                 VStack{
-                  
+                    
                     Button(action: {
                         // QUE DEBE HACER EL BOTTON <--
                         print("Hola Pulsado")
@@ -36,16 +36,96 @@ struct ContentView: View {
                         
                             .padding(12)
                             .overlay (
-                           RoundedRectangle(cornerRadius: 50)
-                           // stroke (relleno)corte(va todo el interior)
-                        .stroke(Color.green,lineWidth: 5)
+                                RoundedRectangle(cornerRadius: 50)
+                                // stroke (relleno)corte(va todo el interior)
+                                    .stroke(Color.green,lineWidth: 5)
                             )
+                        
+                        
                     }
+                    
+                }.padding()
+                
+                
+                // 1.-Boton
+                
+                Button(action: {
+                    print("Boton trash icono pulsado")
+                    
+                }) {
+                    Image(systemName: "trash")
+                    
+                        .font(.largeTitle)
+                        .foregroundColor(.red)
+                }
+                
+                
+                
+                //2.- Boton Background red clipShape
+                Button(action: {
+                    print("Boton con icono pulsada")
+                    
+                }) {
+                    Image(systemName: "trash")
+                    
+                    
+                        .padding()
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .background(.red)
+                        .clipShape(Circle())
+                    
+                }
+                
+                
+                //3.- imagen y texto
+                Button(action: {
+                    print("Boton con icono pulsada")
+                    
+                }) { // incrustar
+                    HStack {
+                        Image(systemName: "trash")
+                        
+                        Text("Eliminar")
+                            .fontWeight(.bold)
+                            .font(.largeTitle)
+                    }// modificando a toda la caja
+                    
+                    .padding()
+                    .background(Color.red)
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .cornerRadius(60)
                 }
                 
             }
+            //4.- Gradientes
+            Button(action: {
+                print("Boton con icono 4 pulsada")
+                
+            }) { // incrustar
+                HStack {
+                    Image(systemName: "trash")
+                    
+                    Text("Eliminar")
+                        .fontWeight(.bold)
+                        .font(.largeTitle)
+                }// modificando a toda la caja
+                // uigradients
+                .padding()
+                .background(LinearGradient(gradient: Gradient(colors: [Color("Quepal-1"),Color("Quepal-2")]),
+                startPoint: .leading, endPoint: .trailing))
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .cornerRadius(60)
+                .shadow(color:.gray,radius: 10.0,x:20,y:3)
+            }
+            
         }
+        
+  
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
